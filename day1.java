4db5d2a3d0ecddb0c4e2b2ebcb6d656de3acda10
akkiiop <kawadeakshay23@gmail.com>
2025-08-15 20:11:42 +0530

    Update day1.java

--- day1.java
+++ day1.java
@@ -1,17 +1,40 @@
-class Soln {
-    public void sort(int[] nums) {
-        int low = 0, mid = 0, high = nums.length - 1;
 
-        while (mid <= high) {
-            if (nums[mid] == 0) {
-                int temp = nums[low]; nums[low] = nums[mid]; nums[mid] = temp;
+
+class sort0s1s2s {
+    public static void sort(int arr[],int n){
+        int low=0;
+        int mid=0;
+        int high=n-1;
+         while (mid <= high) {
+            if (arr[mid] == 0) {
+                int temp = arr[low];
+                arr[low] = arr[mid];
+                arr[mid] = temp;
                 low++; mid++;
-            } else if (nums[mid] == 1) {
+            } else if (arr[mid] == 1) {
                 mid++;
-            } else { 
-                int temp = nums[mid]; nums[mid] = nums[high]; nums[high] = temp;
+            } else {
+                int temp = arr[mid];
+                arr[mid] = arr[high];
+                arr[high] = temp;
                 high--;
             }
         }
+        
+    }
+    public static void main(String[] args) {
+        
+        int[] arr={0,1,2,1,1,0,0,2};
+        int n=arr.length;
+        
+        sort(arr,n);
+
+        System.out.print("[");
+        for (int i = 0; i < n; i++) {
+            System.out.print(arr[i]);
+            if (i < n - 1) System.out.print(", ");
+        }
+        System.out.println("]");
+    
     }
 }
