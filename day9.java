1dca958ee7605177910d15b00a71fda2a6b23740
akkiiop <kawadeakshay23@gmail.com>
2025-08-23 21:25:41 +0530

    Create day9.java

---
+++ day9.java
@@ -0,0 +1,26 @@
+class LongestCommonPrefix {
+    public String longestCommonPrefix(String[] strs) {
+        if (strs == null || strs.length == 0) return "";
+        String prefix = strs[0];
+
+        for (int i = 1; i < strs.length; i++) {
+            while (strs[i].indexOf(prefix) != 0) {
+                prefix = prefix.substring(0, prefix.length() - 1);
+                if (prefix.isEmpty()) return "";
+            }
+        }
+
+        return prefix;
+    }
+
+    
+    public static void main(String[] args) {
+        LongestCommonPrefix lcp = new LongestCommonPrefix();
+
+        String[] input1 = {"flower", "flow", "flight"};
+        System.out.println(lcp.longestCommonPrefix(input1));  
+        
+        String[] input5 = {"alone"};
+        System.out.println(lcp.longestCommonPrefix(input5)); 
+    }
+}
